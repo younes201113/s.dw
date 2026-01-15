d5bc1e64ca19521e75b48a430cc12aeef7aebfaa
younes201113 <yy7351140@gmail.com>
2026-01-15 17:09:49 +0000

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,26 @@
+const games = [
+  {
+    id: 1,
+    name: "GTA San Andreas",
+    category: "pc",
+    genre: ["action", "adventure"],
+    size: 4.0,
+    downloads: 12500,
+    rating: 4.7,
+    image: "images/games/gta.jpg",
+    file: "downloads/gta.zip",
+    desc: "واحدة من أشهر ألعاب العالم المفتوح."
+  },
+  {
+    id: 2,
+    name: "IGI 2",
+    category: "pc",
+    genre: ["action"],
+    size: 1.2,
+    downloads: 8900,
+    rating: 4.3,
+    image: "images/games/igi2.jpg",
+    file: "downloads/igi2.zip",
+    desc: "لعبة تصويب خفيفة للأجهزة الضعيفة."
+  }
+];
