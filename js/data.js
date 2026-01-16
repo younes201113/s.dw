069c34dc6ff5e6c63949025a8080187593cdcde6
younes201113 <yy7351140@gmail.com>
2026-01-16 21:13:16 +0000

    Update data.js

--- js/data.js
+++ js/data.js
@@ -8,6 +8,7 @@ const games = [
     downloads: 120000,
     rating: 4.8,
     image: "files/gta.jpg"
+    download: "https://download2442.mediafire.com/xs8h66u6fdjgJhkKxpxaQ8-lMJuSPychPXGbqn9yBbMRLTrM36dIJ4IjokjU6sNzD_Pb78IQyjghPqhp8Qfzd-MyGrvNSZiY3Mdrvr1snmJR0dAHVawk9EZ5--KDyDainACZK8AJIyUJ1_3HAnu2ky99vu1hcIQGbwPJ0mxj5RcQrxg/6yfprqb5on7uqs5/Free+Download+Files.zip"
   },
   {
     id: 2,
@@ -18,6 +19,7 @@ const games = [
     downloads: 95000,
     rating: 4.6,
     image: "https://uptogames.net/wp-content/uploads/2021/06/Call-of-Duty-4-Modern-Warfare-80x80.webp"
+    download: "https://download2442.mediafire.com/xs8h66u6fdjgJhkKxpxaQ8-lMJuSPychPXGbqn9yBbMRLTrM36dIJ4IjokjU6sNzD_Pb78IQyjghPqhp8Qfzd-MyGrvNSZiY3Mdrvr1snmJR0dAHVawk9EZ5--KDyDainACZK8AJIyUJ1_3HAnu2ky99vu1hcIQGbwPJ0mxj5RcQrxg/6yfprqb5on7uqs5/Free+Download+Files.zip"
   },
   {
     id: 3,
@@ -28,5 +30,6 @@ const games = [
     downloads: 500000,
     rating: 4.9,
     image: "files/winrar.jpg"
+    download: "https://download2442.mediafire.com/xs8h66u6fdjgJhkKxpxaQ8-lMJuSPychPXGbqn9yBbMRLTrM36dIJ4IjokjU6sNzD_Pb78IQyjghPqhp8Qfzd-MyGrvNSZiY3Mdrvr1snmJR0dAHVawk9EZ5--KDyDainACZK8AJIyUJ1_3HAnu2ky99vu1hcIQGbwPJ0mxj5RcQrxg/6yfprqb5on7uqs5/Free+Download+Files.zip"
   }
 ];
