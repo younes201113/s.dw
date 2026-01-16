0b63e28946d3060b52971d89443a4312687d6a47
younes201113 <yy7351140@gmail.com>
2026-01-16 20:56:59 +0000

    Update data.js

--- js/data.js
+++ js/data.js
@@ -4,23 +4,29 @@ const games = [
     name: "GTA San Andreas",
     category: "pc",
     genre: ["action", "adventure"],
-    size: 4.0,
-    downloads: 12500,
-    rating: 4.7,
-    image: "images/games/gta.jpg",
-    file: "downloads/gta.zip",
-    desc: "واحدة من أشهر ألعاب العالم المفتوح."
+    size: 4,
+    downloads: 120000,
+    rating: 4.8,
+    image: "files/gta.jpg"
   },
   {
     id: 2,
-    name: "IGI 1",
+    name: "Call of Duty 4",
     category: "pc",
     genre: ["action"],
-    size: 1.2,
-    downloads: 8900,
-    rating: 4.3,
-    image: "https://uptogames.net/wp-content/uploads/2021/04/IGI-150x150.webp",
-    file: "https://download2430.mediafire.com/0qp9oa1hljsgAm97PuPcTzTNb0iMZ3CvFyTX4VtnilZBtoJlHnU1Icd_RYjJEawnRWaORIHQpgx5CwgU4ZotkzTkadxX8f0ulkmykibC3sTnYviVzHjZaQbwfimVgHJuOokJKgrLJN91wlcxL5Cde1nhe0ZdlX8hjTgYBDpzcPhdVQ/i7ek2uqyr6vcic3/Free+Download+Files.zip",
-    desc: "لعبة تصويب خفيفة للأجهزة الضعيفة."
+    size: 6,
+    downloads: 95000,
+    rating: 4.6,
+    image: "files/cod4.jpg"
+  },
+  {
+    id: 3,
+    name: "WinRAR",
+    category: "software",
+    genre: [],
+    size: 0.01,
+    downloads: 500000,
+    rating: 4.9,
+    image: "files/winrar.jpg"
   }
 ];
