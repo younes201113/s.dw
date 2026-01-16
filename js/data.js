50e1035cc3283b5ae80bf05b92cfff60839b962c
younes201113 <yy7351140@gmail.com>
2026-01-16 21:10:09 +0000

    Update data.js

--- js/data.js
+++ js/data.js
@@ -11,13 +11,13 @@ const games = [
   },
   {
     id: 2,
-    name: "Call of Duty 4",
+    name: "Call of Duty 4 Modern Warfare",
     category: "pc",
     genre: ["action"],
     size: 6,
     downloads: 95000,
     rating: 4.6,
-    image: "files/cod4.jpg"
+    image: "https://uptogames.net/wp-content/uploads/2021/06/Call-of-Duty-4-Modern-Warfare-80x80.webp"
   },
   {
     id: 3,
