b9109b0ff4a9aeb6bb079cb895ae15b7a09b0c3f
younes201113 <yy7351140@gmail.com>
2026-01-17 17:38:36 +0000

    Update data.js

--- data.js
+++ data.js
@@ -10,12 +10,12 @@ const gameData = [
         downloads: 25000000,
         rating: 4.8,
         size: "95 GB",
-        image: "assets/games/gta5.jpg",
+        image: "https://uptogames.net/wp-content/uploads/2021/05/GTA-5-8-150x150.webp",
         screenshots: [
-            "assets/games/gta5_1.jpg",
-            "assets/games/gta5_2.jpg",
-            "assets/games/gta5_3.jpg",
-            "assets/games/gta5_4.jpg"
+            "https://uptogames.net/wp-content/uploads/2021/05/GTA-5-1.webp",
+            "https://uptogames.net/wp-content/uploads/2021/05/GTA-5-2.webp",
+            "https://uptogames.net/wp-content/uploads/2021/05/GTA-5-3.webp",
+            "https://uptogames.net/wp-content/uploads/2021/05/GTA-5-4.webp"
         ],
         specs: {
             cpu: "Intel Core i5-3470 أو AMD FX-8350",
@@ -25,9 +25,9 @@ const gameData = [
             storage: "95 GB مساحة حرة"
         },
         downloadLinks: [
-            { name: "الجزء 1", url: "downloads/gta5_part1.zip", size: "10 GB" },
-            { name: "الجزء 2", url: "downloads/gta5_part2.zip", size: "10 GB" },
-            { name: "الجزء 3", url: "downloads/gta5_part3.zip", size: "10 GB" }
+            { name: "الجزء 1", url: "https://download2435.mediafire.com/jc8ygydc6tkgvI8gmahd1zWoKx6N0PnBS-HDMIsG0x4H0N24VZKrLg91NMBZN5jPWD5sAFgFE359NwpzrXzHCazEvFtvg4Igf1Avlzc8eZDFrB_RXZOSUozQbq6BCXtWZxyIMzFCVbLZzPMSKzFDAAJE7ShLsUjKK5Jnw9NkYnPQ3g/vr10jsh4350pta9/Free+Download+Files.zip", size: "10 GB" },
+            { name: "الجزء 2", url: "https://download2435.mediafire.com/jc8ygydc6tkgvI8gmahd1zWoKx6N0PnBS-HDMIsG0x4H0N24VZKrLg91NMBZN5jPWD5sAFgFE359NwpzrXzHCazEvFtvg4Igf1Avlzc8eZDFrB_RXZOSUozQbq6BCXtWZxyIMzFCVbLZzPMSKzFDAAJE7ShLsUjKK5Jnw9NkYnPQ3g/vr10jsh4350pta9/Free+Download+Files.zip", size: "10 GB" },
+            { name: "الجزء 3", url: "https://download2435.mediafire.com/jc8ygydc6tkgvI8gmahd1zWoKx6N0PnBS-HDMIsG0x4H0N24VZKrLg91NMBZN5jPWD5sAFgFE359NwpzrXzHCazEvFtvg4Igf1Avlzc8eZDFrB_RXZOSUozQbq6BCXtWZxyIMzFCVbLZzPMSKzFDAAJE7ShLsUjKK5Jnw9NkYnPQ3g/vr10jsh4350pta9/Free+Download+Files.zip", size: "10 GB" }
         ],
         featured: true,
         trending: true
@@ -42,11 +42,11 @@ const gameData = [
         downloads: 15000000,
         rating: 4.5,
         size: "20 GB",
-        image: "assets/games/valorant.jpg",
+        image: "https://th.bing.com/th/id/ODF.FFPVJ6dLO7g0Tno32UWDJQ?w=32&h=32&qlt=90&pcl=fffffc&r=0&o=6&pid=1.2",
         screenshots: [
-            "assets/games/valorant_1.jpg",
-            "assets/games/valorant_2.jpg",
-            "assets/games/valorant_3.jpg"
+            "https://th.bing.com/th/id/R.e285b26b018da31c9be91c913e02684e?rik=7s89tfb%2fQBIR5A&pid=ImgRaw&r=0",
+            "https://th.bing.com/th/id/OIP.HZoXzbh99OAz7TKx2yWFlwHaEK?w=315&h=180&c=7&r=0&o=5&pid=1.7",
+            "https://tse3.mm.bing.net/th/id/OIP.OYotQ7KMypGmnb1CL1oIAgHaEK?pid=ImgDet&w=474&h=266&rs=1&o=7&rm=3"
         ],
         specs: {
             cpu: "Intel Core 2 Duo E8400",
@@ -56,7 +56,7 @@ const gameData = [
             storage: "20 GB مساحة حرة"
         },
         downloadLinks: [
-            { name: "النسخة الكاملة", url: "downloads/valorant_setup.exe", size: "20 GB" }
+            { name: "النسخة الكاملة", url: "https://playvalorant.com/ar-ae/", size: "20 GB" }
         ],
         featured: true,
         trending: true
@@ -71,10 +71,10 @@ const gameData = [
         downloads: 1000000000,
         rating: 4.7,
         size: "2 GB",
-        image: "assets/games/pubg_mobile.jpg",
+        image: "https://img.utdstc.com/icon/ad3/e98/ad3e98abe9de44760d7dfe5f65cd833bd059c163c3cdbe5a8703c2e35a5bb02f:100",
         screenshots: [
-            "assets/games/pubg_mobile_1.jpg",
-            "assets/games/pubg_mobile_2.jpg"
+            "https://img.utdstc.com/screen/554/199/554199f937c04389e2089ed74eb2114df70cd15e416f3a1cbb98621a9825baca:200",
+            "https://img.utdstc.com/screen/a89/0e7/a890e724f6ead25ae2796f3585ab4ded117bba6257d4337bdcc65185c69a9fa6:200"
         ],
         specs: {
             android: "Android 5.1.1 أو أحدث",
@@ -82,7 +82,7 @@ const gameData = [
             storage: "2 GB مساحة حرة"
         },
         downloadLinks: [
-            { name: "APK مباشر", url: "downloads/pubg_mobile.apk", size: "2 GB" }
+            { name: "APK مباشر", url: "https://d.apkpure.com/b/APK/com.tencent.ig?version=latest", size: "2 GB" }
         ],
         featured: true,
         trending: true
@@ -97,9 +97,9 @@ const gameData = [
         downloads: 500000000,
         rating: 4.6,
         size: "250 MB",
-        image: "assets/games/clash_of_clans.jpg",
+        image: "https://image.winudf.com/v2/image1/Y29tLnN1cGVyY2VsbC5jbGFzaG9mY2xhbnNfaWNvbl8xNzU5MzIzNDA5XzAwNw/icon.webp?w=320&fakeurl=1&type=.webp",
         screenshots: [
-            "assets/games/clash_of_clans_1.jpg"
+            "https://img.utdstc.com/screen/f49/830/f49830795863850a8e76537b760deb46de57687ece3f82870b673834a55eb2ff:200"
         ],
         specs: {
             android: "Android 4.1 أو أحدث",
@@ -107,8 +107,8 @@ const gameData = [
             storage: "250 MB مساحة حرة"
         },
         downloadLinks: [
-            { name: "نسخة Android", url: "downloads/clash_of_clans.apk", size: "250 MB" },
-            { name: "نسخة iOS", url: "https://apps.apple.com/app/clash-of-clans/id529479190", size: "250 MB" }
+            { name: "نسخة Android", url: "https://apkpure.com/fr/clash-of-clans-android-app/com.supercell.clashofclans/download?utm_content=1008", size: "250 MB" },
+            { name: "نسخة iOS", url: "https://apkpure.com/fr/clash-of-clans-android-app/com.supercell.clashofclans/download?utm_content=1008", size: "250 MB" }
         ],
         featured: true,
         trending: true
@@ -123,10 +123,10 @@ const gameData = [
         downloads: 15000000,
         rating: 4.9,
         size: "3 GB",
-        image: "assets/programs/photoshop.jpg",
+        image: "https://img.utdstc.com/icon/a72/da4/a72da47b9cfb34eac94550bc25fb376dd66066c7ad92aeea4d13e84ac8ec2929:100",
         screenshots: [
-            "assets/programs/photoshop_1.jpg",
-            "assets/programs/photoshop_2.jpg"
+            "https://www.bing.com/th/id/OIP.Ti_NOMGVPVUqLLxFr8swtQHaE5?w=263&h=211&c=8&rs=1&qlt=90&r=0&o=6&pid=3.1&rm=2",
+            "https://www.bing.com/th/id/OIP.sbQQO89rcuM4uJbkM5W09wHaEo?w=277&h=211&c=8&rs=1&qlt=90&r=0&o=6&pid=3.1&rm=2"
         ],
         specs: {
             cpu: "Intel أو AMD مع دعم 64-bit",
@@ -136,8 +136,8 @@ const gameData = [
             storage: "4 GB مساحة حرة"
         },
         downloadLinks: [
-            { name: "النسخة الكاملة", url: "downloads/photoshop_2023.zip", size: "3 GB" },
-            { name: "التفعيل", url: "downloads/photoshop_crack.rar", size: "50 MB" }
+            { name: "النسخة الكاملة", url: "https://adobe-photoshop-express.fr.uptodown.com/android/telecharger", size: "3 GB" },
+            { name: "التفعيل", url: "https://adobe-photoshop-express.fr.uptodown.com/android/telecharger", size: "50 MB" }
         ],
         featured: true,
         trending: true
@@ -152,17 +152,17 @@ const gameData = [
         downloads: 50000000,
         rating: 4.3,
         size: "3 MB",
-        image: "assets/programs/winrar.jpg",
+        image: "https://img.utdstc.com/icon/e60/55f/e6055f932d15a9451379c6bd0eceee301bd94f867dc7377cb2959be9a6861abd:100",
         screenshots: [
-            "assets/programs/winrar_1.jpg"
+            "https://img.utdstc.com/screen/4e5/9d8/4e59d8496d9403e233d350e6f03119f06f6c9d2470a2b07ad87a3312ce57eae8:200"
         ],
         specs: {
             os: "Windows 7/8/10/11",
             storage: "3 MB مساحة حرة"
         },
         downloadLinks: [
-            { name: "إصدار 64-bit", url: "downloads/winrar_x64.exe", size: "3 MB" },
-            { name: "إصدار 32-bit", url: "downloads/winrar_x86.exe", size: "3 MB" }
+            { name: "إصدار 64-bit", url: "https://rar-for-android.fr.uptodown.com/android/telecharger", size: "3 MB" },
+            { name: "إصدار 32-bit", url: "https://rar-for-android.fr.uptodown.com/android/telecharger", size: "3 MB" }
         ],
         trending: true
     },
@@ -176,9 +176,9 @@ const gameData = [
         downloads: 5000000000,
         rating: 4.4,
         size: "80 MB",
-        image: "assets/apps/whatsapp.jpg",
+        image: "https://img.utdstc.com/icon/ce6/4c5/ce64c5fdcab44eb799b11d71e554c06ede50bc8524cc906de9da7c9aed58983b:100",
         screenshots: [
-            "assets/apps/whatsapp_1.jpg"
+            "https://img.utdstc.com/screen/fbf/746/fbf746a46cfe04e688dfeb8c3e610d4aec1338f9d63ea2d0d5428f3aa28f3d99:200"
         ],
         specs: {
             android: "Android 4.1 أو أحدث",
@@ -186,8 +186,8 @@ const gameData = [
             storage: "80 MB مساحة حرة"
         },
         downloadLinks: [
-            { name: "نسخة Android", url: "downloads/whatsapp.apk", size: "80 MB" },
-            { name: "نسخة iOS", url: "https://apps.apple.com/app/whatsapp-messenger/id310633997", size: "80 MB" }
+            { name: "نسخة Android", url: "https://whatsapp-messenger.fr.uptodown.com/android/telecharger", size: "80 MB" },
+            { name: "نسخة iOS", url: "https://whatsapp-messenger.fr.uptodown.com/android/telecharger", size: "80 MB" }
         ],
         featured: true
     },
@@ -201,17 +201,17 @@ const gameData = [
         downloads: 100000000,
         rating: 4.2,
         size: "25 MB",
-        image: "assets/apk/snaptube.jpg",
+        image: "https://img.utdstc.com/icon/b11/7ce/b117ce7ff728bd1bf2883fdffdcb170f487a793a8f65db956649a846861e138c:100",
         screenshots: [
-            "assets/apk/snaptube_1.jpg",
-            "assets/apk/snaptube_2.jpg"
+            "https://img.utdstc.com/screen/8c5/563/8c556358775ce4366841923259ecef405db1a949602a545d4deb1ff9c3f43900:200",
+            "https://img.utdstc.com/screen/826/8df/8268df111e2c911ce053dcacb08449620f659e50b31106e99ad84c4d3b16f12e:200"
         ],
         specs: {
             android: "Android 4.4 أو أحدث",
             storage: "25 MB مساحة حرة"
         },
         downloadLinks: [
-            { name: "الإصدار الأخير", url: "downloads/snaptube_latest.apk", size: "25 MB" }
+            { name: "الإصدار الأخير", url: "https://snaptube.fr.uptodown.com/android/telecharger", size: "25 MB" }
         ],
         trending: true
     },
@@ -225,10 +225,10 @@ const gameData = [
         downloads: 85000000,
         rating: 4.6,
         size: "125 GB",
-        image: "assets/games/cod_warzone.jpg",
+        image: "https://img.utdstc.com/icon/398/3aa/3983aa6b3ad0e1723ab331d6ef41c6ceb9bdd0d6c93acb57b4ce352c06ddc01b:100",
         screenshots: [
-            "assets/games/cod_warzone_1.jpg",
-            "assets/games/cod_warzone_2.jpg"
+            "https://img.utdstc.com/screen/311/b8d/311b8da9ce2867154790855052e7459aa7ad2041d02d8daeb94f83fa80ea698d:200",
+            "https://img.utdstc.com/screen/862/f9b/862f9b9229b96fa59925e8e931ad448382b657a774e7c5b507419448d5608c7a:200"
         ],
         specs: {
             cpu: "Intel Core i5-2500K أو AMD Ryzen R5 1600X",
@@ -238,7 +238,7 @@ const gameData = [
             storage: "125 GB مساحة حرة"
         },
         downloadLinks: [
-            { name: "النسخة الكاملة", url: "downloads/cod_warzone_setup.exe", size: "125 GB" }
+            { name: "النسخة الكاملة", url: "https://call-of-duty-warzone.fr.uptodown.com/android/telecharger", size: "125 GB" }
         ],
         trending: true
     },
@@ -252,10 +252,10 @@ const gameData = [
         downloads: 238000000,
         rating: 4.8,
         size: "1 GB",
-        image: "assets/games/minecraft.jpg",
+        image: "https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/logos/Global-Header_MCCB-Logo_300x51.svg",
         screenshots: [
-            "assets/games/minecraft_1.jpg",
-            "assets/games/minecraft_2.jpg"
+            "https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/screenshots/CREATE_BuildAlmostAnything.png",
+            "https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/screenshots/EXPLORE_PDPScreenshotRefresh2024_multipleBiomes_01.png"
         ],
         specs: {
             cpu: "Intel Core i5-4690",
@@ -265,7 +265,7 @@ const gameData = [
             storage: "4 GB مساحة حرة"
         },
         downloadLinks: [
-            { name: "النسخة الكاملة", url: "downloads/minecraft_full.zip", size: "1 GB" }
+            { name: "النسخة الكاملة", url: "https://tlauncher.org/#dPopup", size: "1 GB" }
         ]
     },
     {
@@ -278,17 +278,17 @@ const gameData = [
         downloads: 25000000,
         rating: 4.7,
         size: "100 MB",
-        image: "assets/programs/vscode.jpg",
+        image: "https://th.bing.com/th/id/ODF.-g0sg8HWLH6lfjy77LRUug?w=32&h=32&qlt=90&pcl=fffffc&r=0&o=6&pid=1.2",
         screenshots: [
-            "assets/programs/vscode_1.jpg"
+            "https://th.bing.com/th/id/ODF.-g0sg8HWLH6lfjy77LRUug?w=32&h=32&qlt=90&pcl=fffffc&r=0&o=6&pid=1.2"
         ],
         specs: {
             os: "Windows 7/8/10, macOS, Linux",
             storage: "200 MB مساحة حرة"
         },
         downloadLinks: [
-            { name: "Windows 64-bit", url: "downloads/vscode_win64.exe", size: "100 MB" },
-            { name: "macOS", url: "downloads/vscode_mac.zip", size: "100 MB" }
+            { name: "Windows 64-bit", url: "https://code.visualstudio.com/Download", size: "100 MB" },
+            { name: "macOS", url: "https://code.visualstudio.com/Download", size: "100 MB" }
         ]
     },
     {
@@ -301,16 +301,16 @@ const gameData = [
         downloads: 50000000,
         rating: 4.5,
         size: "50 MB",
-        image: "assets/apk/spotify.jpg",
+        image: "https://img.utdstc.com/icon/4ba/1ef/4ba1ef03c48c81bb20965a85d244c07ec086c20f4ec9a047828e19ecbf245490:100",
         screenshots: [
-            "assets/apk/spotify_1.jpg"
+            "https://img.utdstc.com/screen/9b5/788/9b5788ebd556711506eb976a4fda9f20a8b3ddeb84199f6bbc51708c6f8fd276:200"
         ],
         specs: {
             android: "Android 5.0 أو أحدث",
             storage: "50 MB مساحة حرة"
         },
         downloadLinks: [
-            { name: "Spotify Premium APK", url: "downloads/spotify_premium.apk", size: "50 MB" }
+            { name: "Spotify Premium APK", url: "https://spotify.fr.uptodown.com/android/telecharger", size: "50 MB" }
         ]
     }
 ];
